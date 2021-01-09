c39cf3976ad142e8ed3c8e9a81bc6994d5dce04c
Luka Vieira <lukavieira14@gmail.com>
2021-01-09 18:41:10 -0300

    Receber um arquivo txt e mostrar quantas vogais tem no texto

--- Seção 12/EX_3.py
+++ Seção 12/EX_3.py
@@ -2,6 +2,5 @@ with open('texto.txt', 'w+') as texto:
       texto.write(input('Insira um Texto: '))
       texto.seek(0)
       vogais = 'aeiouAEIOU'
-      #string = texto.readline()
       qtd_vogais = len([c for c in texto.readline() if c in vogais])
       print(qtd_vogais)
